e22e9aaf0076031693738bbf8054e8804b6dea5f
kimbley <kimbley@shardeum.org>
2022-12-08 06:28:20 -0600

    added basic status-api functions

--- src/index.ts
+++ src/index.ts
@@ -37,11 +37,51 @@ dashboard
 
 const gui = program.command('gui').description('GUI related commands');
 
+interface ProcessStatus {
+  name?: string;
+  uptimeInSeconds?: number;
+  restarts?: number;
+  status?: string;
+  cpuUsagePercent?: number;
+  memUsedInBytes?: number;
+  link?: string;
+}
+
 gui
   .command('status')
   .description(
     'Show if GUI is running or not; also the port and URL to connect to it'
-  );
+  )
+  .action(() => {
+    pm2.describe('operator-gui', (err, descriptions) => {
+      if (err) {
+        console.error(err);
+        return pm2.disconnect();
+      }
+      if (descriptions.length == 0) {
+        console.log('operator gui not running!');
+        return pm2.disconnect();
+      }
+      const description = descriptions[0];
+      const status: ProcessStatus = statusFromPM2(description);
+      status.link = 'http://localhost:8080/';
+      console.log(status);
+      return pm2.disconnect();
+    });
+  });
+
+function statusFromPM2(pm2Description: any): ProcessStatus {
+  return {
+    name: pm2Description.name,
+    uptimeInSeconds: pm2Description.pm2_env?.pm_uptime
+      ? (Date.now() - pm2Description.pm2_env?.pm_uptime) / 1000
+      : 0,
+    restarts: pm2Description.pm2_env?.restart_time,
+    status: pm2Description.pm2_env?.status,
+    cpuUsagePercent: pm2Description.monit?.cpu,
+    memUsedInBytes: pm2Description.monit?.memory,
+  };
+}
 
 gui
   .command('start')
@@ -92,7 +132,23 @@ program
   .command('status')
   .description(
     'Show if validator is running or not; also the port and URL to connect to it'
-  );
+  )
+  .action(() => {
+    pm2.describe('validator', (err, descriptions) => {
+      if (err) {
+        console.error(err);
+        return pm2.disconnect();
+      }
+      if (descriptions.length == 0) {
+        console.log('validator not running!');
+        return pm2.disconnect();
+      }
+      const description = descriptions[0];
+      const status: ProcessStatus = statusFromPM2(description);
+      console.log(status);
+      return pm2.disconnect();
+    });
+  });
 
 program
   .command('start')
