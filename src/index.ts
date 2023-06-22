c7d2202114412336a540bd465d6301f5dd280bbf
CombatPug <p.ladar@icloud.com>
2023-06-22 15:07:16 -0300

    fix: set cwd to build folder + absolute path for logger files

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ import cliPackageJson = require('../package.json');
 import dotenv = require('dotenv');
 import {registerGuiCommands} from './gui-commands';
 import {registerNodeCommands} from './node-commands';
+import path = require('path');
 
 dotenv.config();
 
+process.chdir(path.join(__dirname, '..')); // specify the path to the build folder
 const program = new Command();
 
 program
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,18 @@
 import winston from 'winston';
+import path from 'path';
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   defaultMeta: {service: 'node'},
   transports: [
-    new winston.transports.File({filename: 'logs/error.log', level: 'error'}),
-    new winston.transports.File({filename: 'logs/combined.log'}),
+    new winston.transports.File({
+      filename: path.join(__dirname, '../../logs/error.log'),
+      level: 'error',
+    }),
+    new winston.transports.File({
+      filename: path.join(__dirname, '../../logs/combined.log'),
+    }),
   ],
 });
 
