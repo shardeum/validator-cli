6d847cffb30ff74235d53c374b0e643278772013
CombatPug <p.ladar@icloud.com>
2023-06-22 11:54:11 -0300

    fix: cwd set to build (start/stop validator from any directory)

--- environment.config.js
+++ environment.config.js
@@ -5,7 +5,7 @@ module.exports = [
     name: 'validator',
     script: path.join(__dirname, 'auto-restart-controller.js'),
     output: path.join(__dirname, './validator-logs.txt'),
-    // cwd: path.join(__dirname, './build'),
+    cwd: path.join(__dirname, './build'),
     autorestart: false, // set this to false because we have our own restart controller
   },
 ];
