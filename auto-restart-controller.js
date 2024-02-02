be5beaf55e64a1f22070254f2eaecb9b65839e2b
Tanuj Soni <tanuj@shardeum.org>
2024-02-02 15:54:37 +0530

    Add exit code to exit summary

--- auto-restart-controller.js
+++ auto-restart-controller.js
@@ -16,26 +16,27 @@ const updateAutoRestartStatus = () => {
   }
 };
 
-const updateExitSummaryWithAutoRestart = (autoRestartStatus) => {
+const updateExitSummary = (autoRestartStatus, exitCode) => {
   const exitSummaryFileName = 'exit-summary.json';
   const logsDirectory = './build/logs';
   const exitSummaryDataPath = path.join(__dirname, logsDirectory, exitSummaryFileName);
 
   try {
-    console.log('Adding auto restart status to exit summary...');
+    console.log('Adding auto restart status and exit code to exit summary...');
 
     // eslint-disable-next-line security/detect-non-literal-fs-filename
     const exitSummaryData = fs.readFileSync(exitSummaryDataPath).toString();
     const exitSummaryDataJson = JSON.parse(exitSummaryData);
 
     exitSummaryDataJson.autoRestart = autoRestartStatus;
+    exitSummaryDataJson.exitCode = exitCode;
 
     // eslint-disable-next-line security/detect-non-literal-fs-filename
     fs.writeFileSync(exitSummaryDataPath, JSON.stringify(exitSummaryDataJson, null, 2));
 
-    console.log('Successfully updated exit summary with auto restart status.');
+    console.log('Successfully updated exit summary with auto restart status and exit code.');
   } catch (error) {
-    console.error(`Error adding auto restart status to exit summary: ${error}`);
+    console.error(`Error updating exit summary: ${error}`);
   }
 };
 
@@ -62,7 +63,7 @@ const startApp = () => {
       `Child process exited with code ${code}. shouldAutoRestart: `,
       shouldAutoRestart
     );
-    updateExitSummaryWithAutoRestart(shouldAutoRestart);
+    updateExitSummary(shouldAutoRestart, code);
     if (shouldAutoRestart && code === 0) {
       console.log('Restarting validator');
       startApp();
